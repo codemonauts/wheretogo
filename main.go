4465583848856289036d306ed0c16a55e3b2056f
Felix Breidenstein <mail@felixbreidenstein.de>
2018-11-04 19:32:38 +0100

    First basic featureset

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-	"os"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"regexp"
 
-	"github.com/BurntSushi/toml"
+	"github.com/gorilla/mux"
+	"gopkg.in/yaml.v2"
 )
 
 func check(e error) {
@@ -12,33 +17,60 @@ func check(e error) {
 	}
 }
 
+type Route struct {
+	Path   string `json:"path"`
+	Match  string `json:"match"`
+	Target string `json:"target"`
+}
+
+type Entry struct {
+	Name   string  `json:"name"`
+	Domain string  `json:"domain"`
+	Routes []Route `json:"routes"`
+}
+
+type Config struct {
+	Entrys []Entry `json:"entrys"`
+}
+
+func getRedirectFunc(route Route) func(w http.ResponseWriter, r *http.Request) {
+	// Compile regexp to match path during start and save in a closure
+	re := regexp.MustCompile(route.Match)
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		var destination = re.ReplaceAllString(r.URL.Path, route.Target)
+		http.Redirect(w, r, destination, 301)
+	}
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s%s\n", r.Host, r.RequestURI)
+		x := mux.CurrentRoute(r)
+		log.Println(x.GetName())
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
-	var tomlData = `
-		[feature1]
-		enable = true
-		[feature2]
-		enable = false`
-
-	type feature struct {
-		Enable bool
-	}
-
-	type tomlConfig struct {
-		Title string
-		F1    feature `toml:"feature1"`
-		F2    feature `toml:"feature2"`
-	}
-
-	var conf tomlConfig
-	if _, err := toml.Decode(tomlData, &conf); err != nil {
-		panic(err)
-	}
-
-	f, err := os.Create("testfile")
+	dat, err := ioutil.ReadFile("config.yml")
+	check(err)
+	c := &Config{}
+	err = yaml.Unmarshal(dat, &c)
 	check(err)
 
-	e := toml.NewEncoder(f)
-	err = e.Encode(conf)
-	check(err)
+	r := mux.NewRouter()
+	r.Use(loggingMiddleware)
 
+	for _, e := range c.Entrys {
+		fmt.Printf("> Loading rule: %q\n", e.Name)
+		s := r.Host(e.Domain).Subrouter()
+		for _, r := range e.Routes {
+			s.PathPrefix(r.Path).HandlerFunc(getRedirectFunc(r)).Name(e.Name)
+		}
+	}
+
+	http.Handle("/", r)
+
+	panic(http.ListenAndServe(":9090", nil))
 }
