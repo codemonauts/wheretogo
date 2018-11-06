1af4b9ee8bb4d791131fc466837745dde0eb8df7
Felix Breidenstein <mail@felixbreidenstein.de>
2018-11-06 02:14:37 +0100

    Added type:'Webhook' redirects

--- main.go
+++ main.go
@@ -17,37 +17,57 @@ func check(e error) {
 	}
 }
 
-type Route struct {
-	Path   string `json:"path"`
+type RegexData struct {
 	Match  string `json:"match"`
 	Target string `json:"target"`
 }
+type WebhookData struct {
+	Endpoint string `json:"endpoint"`
+}
 
 type Entry struct {
-	Name   string  `json:"name"`
-	Domain string  `json:"domain"`
-	Routes []Route `json:"routes"`
+	Name   string            `json:"name"`
+	Domain string            `json:"domain"`
+	Type   string            `json:"type"`
+	Path   string            `json:"path"`
+	Data   map[string]string `json:"data"`
 }
 
 type Config struct {
 	Entrys []Entry `json:"entrys"`
 }
 
-func getRedirectFunc(route Route) func(w http.ResponseWriter, r *http.Request) {
+func getRegExHandler(data map[string]string) func(w http.ResponseWriter, r *http.Request) {
 	// Compile regexp to match path during start and save in a closure
-	re := regexp.MustCompile(route.Match)
+	re := regexp.MustCompile(data["match"])
 
 	return func(w http.ResponseWriter, r *http.Request) {
-		var destination = re.ReplaceAllString(r.URL.Path, route.Target)
+		var destination = re.ReplaceAllString(r.URL.Path, data["target"])
 		http.Redirect(w, r, destination, 301)
+	}
+}
+
+func getWebhookHandler(data map[string]string) func(w http.ResponseWriter, r *http.Request) {
+
+	client := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(data["endpoint"])
+		request, err := client.Get(data["endpoint"])
+		check(err)
+		destination, _ := request.Location()
+		http.Redirect(w, r, destination.String(), 301)
 	}
 }
 
 func loggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s%s\n", r.Host, r.RequestURI)
 		x := mux.CurrentRoute(r)
-		log.Println(x.GetName())
+		log.Printf("[%s] %s%s\n", x.GetName(), r.Host, r.RequestURI)
 		next.ServeHTTP(w, r)
 	})
 }
@@ -65,12 +85,14 @@ func main() {
 	for _, e := range c.Entrys {
 		fmt.Printf("> Loading rule: %q\n", e.Name)
 		s := r.Host(e.Domain).Subrouter()
-		for _, r := range e.Routes {
-			s.PathPrefix(r.Path).HandlerFunc(getRedirectFunc(r)).Name(e.Name)
+		switch e.Type {
+		case "regex":
+			s.PathPrefix(e.Path).HandlerFunc(getRegExHandler(e.Data)).Name(e.Name)
+		case "webhook":
+			s.PathPrefix(e.Path).HandlerFunc(getWebhookHandler(e.Data)).Name(e.Name)
 		}
 	}
 
 	http.Handle("/", r)
-
 	panic(http.ListenAndServe(":9090", nil))
 }
